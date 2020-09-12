e38061bd1486d1e87e9a1d577233314d604fb6f2
sgibb1 <35509885+sgibb1@users.noreply.github.com>
2020-09-11 21:12:49 -0400

    Add files via upload

---
+++ JavaScriptAssessment/index.js
@@ -0,0 +1,25 @@
+var firstName = "Shaun";
+
+var lastName = "Gibb";
+
+var age = "37";
+
+// Could also be done like this
+//var firstName, lastName, age;
+//firstName = Shaun;
+//lastName = Gibb;
+//age = 37
+
+alert("My name is " + firstName + " " + lastName + ".");
+
+alert("I am " + age + " years old.");
+
+if (age > 21) {
+  alert("I am legally able to drink in the states.");
+} else if (age == 21) {
+  alert("I am just barely able to drink legally in the states.");
+} else if (age < 21) {
+  alert("It is illegal for me to drink in the states.");
+}
+
+console.log("I'm enjoying this class.");
